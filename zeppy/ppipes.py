cb01bbe0a1480be68bb4d7b1f1943651e94542c2
santoshphilip <santosh_philip@yahoo.com>
2021-01-24 08:45:56 -0800

    added license to top of files

--- zeppy/ppipes.py
+++ zeppy/ppipes.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2020 Santosh Philip
+# =======================================================================
+#  Distributed under the MIT License.
+#  (See accompanying file LICENSE or copy at
+#  http://opensource.org/licenses/MIT)
+# =======================================================================
+# -*- coding: utf-8 -*-
 """parallel pipeline functions"""
 
 
